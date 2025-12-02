cfd9adb135d960457c73e223fef70088f40b03ca
theramiway <ramyarajeshrao1411@gmail.com>
2025-12-02 20:38:53 +0530

    Update api Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -13,7 +13,7 @@ export default function Register() {
     setIsSubmitting(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
+      const res = await fetch("https://fintelify.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
@@ -111,3 +111,4 @@ export default function Register() {
     </div>
   );
 }
+
