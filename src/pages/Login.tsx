80e1941c131c611650f47712c10c9711af806012
theramiway <ramyarajeshrao1411@gmail.com>
2025-12-02 20:38:16 +0530

    Update api Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,7 +13,7 @@ export default function Login() {
     setIsSubmitting(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://fintelify.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
@@ -114,3 +114,4 @@ export default function Login() {
     </div>
   );
 }
+
